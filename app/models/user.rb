f208d5e46a0c218ed1c49fc83b0f2d45ba38efd2
Jose Reyes <jore5523@gmail.com>
2017-09-04 23:08:06 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  #database association
   belongs_to :plan
+  has_one :profile
+  
   attr_accessor :stripe_card_token
   def save_with_payment
     if valid?
